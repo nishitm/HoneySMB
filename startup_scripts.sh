e4b45445ece3fd347468ce33a82f8c40e0161492
Nishitm@iitk.ac.in <Nishitm@iitk.ac.in>
2017-04-13 11:43:32 +0530

    added files for merging

--- startup_scripts.sh
+++ startup_scripts.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 python /home/smb/smbserver.py &  
+<<<<<<< HEAD
+<<<<<<< HEAD
 tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/smblog.pcap
+=======
+tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/"smblog.pcap"
+>>>>>>> HoneySMB/master
+=======
+tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/smblog.pcap
+>>>>>>> HoneySMB/master
